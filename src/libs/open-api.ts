1957898fc6efc43a5b9b159abca854b16a2ce0f7
Arpon <a4arpon@gmail.com>
2024-11-05 17:56:23 +0600

    redis added

--- src/libs/open-api.ts
+++ src/libs/open-api.ts
@@ -1,53 +1,29 @@
-import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi"
+import {
+  OpenApiGeneratorV3,
+  OpenAPIRegistry,
+} from "@asteasolutions/zod-to-openapi"
 
-const openAPI = new OpenAPIHono()
+export const registry = new OpenAPIRegistry()
 
-const route = createRoute({
-  method: "get",
-  path: "/users/{id}",
-  request: {
-    params: z.object({
-      id: z.string(),
-    }),
-    body: {
-      content: {
-        "application/json": {
-          schema: z.object({
-            id: z.string(),
-          }),
-        },
-      },
-    },
-  },
+registry.registerPath({
+  path: "/users",
+  method: "post",
+  tags: ["admin"],
   responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: z.object({
-            id: z.string(),
-          }),
-        },
-      },
-      description: "Retrieve the user",
+    "200": {
+      description: "dff",
     },
   },
 })
 
-openAPI.openapi(route, (c) => {
-  const { id } = c.req.valid("param")
-  return c.json({
-    id,
-    age: 20,
-    name: "Ultra-man",
-  })
-})
+const generator = new OpenApiGeneratorV3(registry.definitions)
 
-openAPI.doc("/swagger-doc", {
+export const swaggerDocument = generator.generateDocument({
   openapi: "3.0.0",
   info: {
     version: "1.0.0",
-    title: "Hono OpenAPI",
+    title: "My API",
+    description: "This is the API",
   },
+  servers: [{ url: "v1" }],
 })
-
-export { openAPI }
