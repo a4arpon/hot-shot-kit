0880a90779fc05d344841ca4c2aa23496aa09739
Arpon <a4arpon@gmail.com>
2024-11-08 11:52:18 +0600

    new api spec added

--- src/open-api/author.openapi.ts
+++ src/open-api/author.openapi.ts
@@ -1,6 +1,26 @@
 import type { ApiSpecs, UseOpenApi } from "#libs/open-api"
 import { z } from "zod"
 
+/*
+ * ------------------------------------------------------------------------
+ * Open API Specs
+ *
+ * Some Key Points -
+ * 1. To use path params, you need to use the path param syntax in the path
+ * and the path param name in the pathParams array.
+ * Example: path: "/author/{authorSlug}", pathParams: ["authorSlug"]
+ * Second brackets {} are used to define the path param syntax.
+ *
+ * 2. To define a request body, you need to define it as a ZodSchema and
+ * pass it to the requestBody property.
+ * Example: requestBody: z.object({ title: z.string() })
+ *
+ * Tips: In this project we are using Drizzle-ORM, so we can easilty inherit
+ * the ZodSchema from the drizzle-orm.
+ * Doc Link: https://orm.drizzle.team/docs/zod
+ * ------------------------------------------------------------------------
+ */
+
 export class AuthorOpenApiSpecs implements UseOpenApi {
   public readonly specs: ApiSpecs[]
 
@@ -24,7 +44,7 @@ export class AuthorOpenApiSpecs implements UseOpenApi {
 
   private getItem: ApiSpecs = {
     method: "GET",
-    path: "/author/:blogSlug",
+    path: "/author/{authorSlug}",
     pathParams: ["authorSlug"],
     tags: ["Author"],
     summery: "Get single Author from server",
@@ -43,7 +63,7 @@ export class AuthorOpenApiSpecs implements UseOpenApi {
 
   private patchItem: ApiSpecs = {
     method: "PATCH",
-    path: "/author/:blogSlug",
+    path: "/author/{authorSlug}",
     pathParams: ["authorSlug"],
     queryParams: ["authorStatus"],
     tags: ["Author"],
@@ -52,7 +72,7 @@ export class AuthorOpenApiSpecs implements UseOpenApi {
 
   private updateItem: ApiSpecs = {
     method: "PUT",
-    path: "/author/:authorSlug",
+    path: "/author/{authorSlug}",
     pathParams: ["authorSlug"],
     tags: ["Author"],
     summery: "Update  Author",
@@ -63,7 +83,7 @@ export class AuthorOpenApiSpecs implements UseOpenApi {
 
   private deleteItem: ApiSpecs = {
     method: "DELETE",
-    path: "/author/:authorSlug",
+    path: "/author/{authorSlug}",
     pathParams: ["authorSlug"],
     tags: ["Author"],
     summery: "Delete single Author",
